d90eb578a624bccaa01fbb4140e09680287e4ecf
Brian Henry <BrianHenryIE@gmail.com>
2022-04-19 16:37:04 -0700

    Add comments

--- src/WooCommerce/class-thank-you.php
+++ src/WooCommerce/class-thank-you.php
@@ -12,11 +12,20 @@ use Nullcorps\WC_Gateway_Bitcoin\API\API_Interface;
 use WC_Order;
 
 class Thank_You {
+
 	const TEMPLATE_NAME = 'checkout/thankyou-bitcoin-instructions-status.php';
 
+	/**
+	 * Used to check is the gateway relevant for this thank you page load.
+	 *
+	 * @uses API_Interface::is_order_has_bitcoin_gateway()
+	 *
+	 * @var API_Interface
+	 */
 	protected API_Interface $api;
 
 	/**
+	 * Constructor
 	 *
 	 * @param API_Interface $api The main plugin functions.
 	 */
@@ -25,12 +34,14 @@ class Thank_You {
 	}
 
 	/**
+	 * When the thank you page loads, if the order loading is a Bitcoin order, print the payment instructions (via
+	 * the template).
 	 *
 	 * @hooked woocommerce_thankyou
 	 *
-	 * @param int $order_id
+	 * @param int $order_id The order if of the (presumably new) order.
 	 *
-	 * @return void
+	 * @return void Prints its output.
 	 */
 	public function print_instructions( int $order_id ): void {
 
@@ -38,7 +49,12 @@ class Thank_You {
 			return;
 		}
 
-		/** @var WC_Order $order */
+		/**
+		 * No need to check again does `wc_get_order()` return a `WC_Order` object because `API::is_order_has_bitcoin_gateway()`
+		 * already has.
+		 *
+		 * @var WC_Order $order
+		 */
 		$order = wc_get_order( $order_id );
 
 		$order_details = $this->api->get_order_details( $order, false );
