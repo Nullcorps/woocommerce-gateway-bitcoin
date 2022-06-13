870c9a832f719f5692841d4c8ce4eaec21619a5b
Brian Henry <BrianHenryIE@gmail.com>
2022-06-13 16:51:14 -0700

    More address generation tests

--- src/API/Bitcoin/class-bitwasp-api.php
+++ src/API/Bitcoin/class-bitwasp-api.php
@@ -2,12 +2,15 @@
 /**
  * The local PHP library for generating addresses.
  *
+ * @see https://github.com/Bit-Wasp/bitcoin-php
+ * @see https://gist.github.com/mariodian/5b67a1f315a74a7753a6f23d0198ec48
+ *
  * @package    nullcorps/woocommerce-gateway-bitcoin
  */
 
 namespace Nullcorps\WC_Gateway_Bitcoin\API\Bitcoin;
 
-use Nullcorps\WC_Gateway_Bitcoin\API\Address_Storage;
+use Exception;
 use Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Address\AddressCreator;
 use Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Bitcoin;
 use Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Key\Deterministic\HdPrefix\GlobalPrefixConfig;
@@ -21,29 +24,34 @@ use Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Serializer\Key\HierarchicalKey\
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 
+/**
+ * Use Bitwasp API to generate public addresses.
+ */
 class BitWasp_API implements Generate_Address_API_Interface {
 	use LoggerAwareTrait;
 
-	protected Address_Storage $address_storage;
-
-	public function __construct( Address_Storage $address_storage, LoggerInterface $logger ) {
+	/**
+	 * Constructor.
+	 *
+	 * @param LoggerInterface $logger PSR Logger.
+	 */
+	public function __construct( LoggerInterface $logger ) {
 		$this->setLogger( $logger );
-		$this->address_storage = $address_storage;
 	}
 
 	/**
-	 * @throws \Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Exceptions\InvalidNetworkParameter
-	 * @throws \Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Exceptions\RandomBytesFailure
-	 * @throws \Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Exceptions\Base58ChecksumFailure
-	 * @throws \Nullcorps\WC_Gateway_Bitcoin\BitWasp\Buffertools\Exceptions\ParserOutOfRange
-	 * @throws \Nullcorps\WC_Gateway_Bitcoin\BitWasp\Bitcoin\Exceptions\DisallowedScriptDataFactoryException
-	 * @throws \Exception
+	 * Generate the nth address for the given xpub|ypub|zpub.
+	 *
+	 * @param string $public_address The wallet address.
+	 * @param int    $nth Derive path nth address in sequence.
+	 *
+	 * @return string
+	 * @throws Exception Failed to generate address.
 	 */
 	public function generate_address( string $public_address, int $nth ): string {
 
 		$path = "0/$nth";
 
-		// https://gist.github.com/mariodian/5b67a1f315a74a7753a6f23d0198ec48
 		$adapter          = Bitcoin::getEcAdapter();
 		$helper           = new KeyToScriptHelper( $adapter );
 		$slip132          = new Slip132( $helper );
@@ -51,16 +59,33 @@ class BitWasp_API implements Generate_Address_API_Interface {
 
 		switch ( substr( $public_address, 0, 4 ) ) {
 			case 'xpub':
+				/**
+				 * Pay-to-Pubkey Hash.
+				 *
+				 * TODO: This also applies to 'tpub'?
+				 *
+				 * @see https://en.bitcoinwiki.org/wiki/Pay-to-Pubkey_Hash
+				 */
 				$pub_prefix = $slip132->p2pkh( $bitcoin_prefixes );
 				break;
 			case 'ypub':
+				/**
+				 * TODO: Pay To Script Hash - Pay to Witness Script Hash ?
+				 */
 				$pub_prefix = $slip132->p2shP2wpkh( $bitcoin_prefixes );
 				break;
 			case 'zpub':
+				/**
+				 * Pay to Witness Public Key Hash.
+				 *
+				 * TODO: This also applies to 'vpub'?
+				 *
+				 * @see https://programmingblockchain.gitbook.io/programmingblockchain/other_types_of_ownership/p2wpkh_pay_to_witness_public_key_hash
+				 */
 				$pub_prefix = $slip132->p2wpkh( $bitcoin_prefixes );
 				break;
 			default:
-				throw new \Exception( 'Bad public key' );
+				throw new Exception( 'Bad public key' );
 		}
 
 		$network = NetworkFactory::bitcoin();
--- tests/unit/API/Bitcoin/class-bitwasp-api-unit-Test.php
+++ tests/unit/API/Bitcoin/class-bitwasp-api-unit-Test.php
@@ -3,7 +3,6 @@
 namespace Nullcorps\WC_Gateway_Bitcoin\API\Bitcoin;
 
 use BrianHenryIE\ColorLogger\ColorLogger;
-use Nullcorps\WC_Gateway_Bitcoin\API\Address_Storage;
 
 /**
  * @coversDefaultClass \Nullcorps\WC_Gateway_Bitcoin\API\Bitcoin\BitWasp_API
@@ -14,37 +13,69 @@ class BitWasp_API_Unit_Test extends \Codeception\Test\Unit {
 	 * @covers ::generate_address
 	 * @covers ::__construct
 	 */
-	public function test_generate_addresses(): void {
+	public function test_generate_addresses_xpub_1(): void {
 
-		$logger          = new ColorLogger();
-		$address_storage = $this->makeEmpty( Address_Storage::class );
+		$logger = new ColorLogger();
 
-		$sut = new BitWasp_API( $address_storage, $logger );
+		$sut = new BitWasp_API( $logger );
 
-		$public_address = 'zpub6n37hVDJHFyDG1hBERbMBVjEd6ws6zVhg9bMs5STo21i9DgDE9Z9KTedtGxikpbkaucTzpj79n6Xg8Zwb9kY8bd9GyPh9WVRkM55uK7w97K';
-
-		$result = $sut->generate_address( $public_address, 0 );
-
-		$this->assertEquals( 'bc1qzs6ttahakr604009st6vzgkjzx670uwvnfldcn', $result );
-	}
-
-	/**
-	 * The address for this test case was generated first in Electrum.
-	 *
-	 * @covers ::generate_address
-	 */
-	public function test_address_generated_by_electrum(): void {
-
-		$logger          = new ColorLogger();
-		$address_storage = $this->makeEmpty( Address_Storage::class );
-
-		$sut = new BitWasp_API( $address_storage, $logger );
-
-		$public_address = 'zpub6n37hVDJHFyDG1hBERbMBVjEd6ws6zVhg9bMs5STo21i9DgDE9Z9KTedtGxikpbkaucTzpj79n6Xg8Zwb9kY8bd9GyPh9WVRkM55uK7w97K';
+		$public_address = $_ENV['XPUB'];
 
 		$result = $sut->generate_address( $public_address, 1 );
 
-		$this->assertEquals( 'bc1qgvm4wf9c79e59vsw0t8x6peqrdmxr3dju9jv8e', $result );
+		$this->assertEquals( $_ENV['XPUB_DERIVED_1'], $result );
+	}
+
+	/**
+	 * @covers ::generate_address
+	 * @covers ::__construct
+	 */
+	public function test_generate_addresses_xpub_10(): void {
+
+		$logger = new ColorLogger();
+
+		$sut = new BitWasp_API( $logger );
+
+		$public_address = $_ENV['XPUB'];
+
+		$result = $sut->generate_address( $public_address, 10 );
+
+		$this->assertEquals( $_ENV['XPUB_DERIVED_10'], $result );
+	}
+
+	/**
+	 * @covers ::generate_address
+	 * @covers ::__construct
+	 */
+	public function test_generate_addresses_zpub_1(): void {
+
+		$logger = new ColorLogger();
+
+		$sut = new BitWasp_API( $logger );
+
+		$public_address = $_ENV['ZPUB'];
+
+		$result = $sut->generate_address( $public_address, 1 );
+
+		$this->assertEquals( $_ENV['ZPUB_DERIVED_1'], $result );
+	}
+
+
+	/**
+	 * @covers ::generate_address
+	 * @covers ::__construct
+	 */
+	public function test_generate_addresses_zpub_10(): void {
+
+		$logger = new ColorLogger();
+
+		$sut = new BitWasp_API( $logger );
+
+		$public_address = $_ENV['ZPUB'];
+
+		$result = $sut->generate_address( $public_address, 10 );
+
+		$this->assertEquals( $_ENV['ZPUB_DERIVED_10'], $result );
 	}
 
 }
