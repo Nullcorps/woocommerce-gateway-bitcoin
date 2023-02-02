cc9e9ddd925144abf7fae2eae673c1023ff8cfc7
Brian Henry <BrianHenryIE@gmail.com>
2023-02-02 14:23:55 -0800

    Test unscheduling payment check upon payment!

---
+++ tests/integration/woocommerce/class-order-integration-Test.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BrianHenryIE\WC_Bitcoin_Gateway\WooCommerce;
+
+use BrianHenryIE\WC_Bitcoin_Gateway\Action_Scheduler\Background_Jobs;
+
+/**
+ * @coversNothing
+ */
+class Order_Integration_Test extends \Codeception\TestCase\WPTestCase {
+
+	/**
+	 * It seemed the check-unpaid-order job was not being cancelled when the order was paid, but the test
+	 * passed without modifying anything.
+	 */
+	public function test_order_payment_cancels_scheduled_task(): void {
+
+		$order = new \WC_Order();
+		$order->set_payment_method( 'bitcoin_gateway' );
+		$order_id = $order->save();
+
+		$order->set_status( 'on-hold' );
+		$order->save();
+
+		$hook = Background_Jobs::CHECK_UNPAID_ORDER_HOOK;
+		$args = array( 'order_id' => $order_id );
+
+		assert( as_has_scheduled_action( $hook, $args ) );
+
+		$order->payment_complete();
+		$order->save();
+
+		$this->assertFalse( as_has_scheduled_action( $hook, $args ) );
+	}
+
+}
