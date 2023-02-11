bdbf481f00bac1971b19f8450953273d52c6f1ac
Brian Henry <BrianHenryIE@gmail.com>
2023-02-10 20:29:15 -0800

    lint

--- src/interface-settings-interface.php
+++ src/interface-settings-interface.php
@@ -1,30 +1,31 @@
 <?php
 /**
+ * Required settings for the plugin.
+ *
  * @package    brianhenryie/bh-wc-bitcoin-gateway
  */
 
 namespace BrianHenryIE\WC_Bitcoin_Gateway;
 
+/**
+ * Typed settings, presumably saved in `wp_options`.
+ */
 interface Settings_Interface {
 
 	/**
 	 * Needed to add links on plugins.php.
-	 *
-	 * @return string
 	 */
 	public function get_plugin_basename(): string;
 
 	/**
 	 * Semver plugin version for caching of js + css.
 	 * Version compare during upgrade.
-	 *
-	 * @return string
 	 */
 	public function get_plugin_version(): string;
 
 	/**
 	 * The URL to the plugin folder.
-	 * E.g. `https://example.org/wp-content/plugins/bh-wc-bitcoin-gateway`.
+	 * E.g. `https://example.org/wp-content/plugins/bh-wc-bitcoin-gateway/`.
 	 *
 	 * Has trailing slash.
 	 */
