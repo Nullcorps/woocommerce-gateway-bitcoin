1a72384dd02bb3f51068a0fc7229d40f3d250f31
Brian Henry <BrianHenryIE@gmail.com>
2025-09-16 19:53:05 -0700

    Remove duplicated parameters

--- src/frontend/woocommerce/blocks/order-confirmation/exchange-rate/index.tsx
+++ src/frontend/woocommerce/blocks/order-confirmation/exchange-rate/index.tsx
@@ -11,26 +11,18 @@ import { Edit } from './edit';
 
 registerBlockType( metadata.name, {
 	...metadata,
-	title: 'Bitcoin test exchange rate',
-	icon: 'smiley',
-	usesContext: [
-		'bh-wp-bitcoin-gateway/exchangeRate',
-		'bh-wp-bitcoin-gateway/exchangeRateUrl',
-	],
-	supports: {
-		// Removes support for an HTML mode.
-		html: false,
-	},
 	edit: Edit,
 	save: ( { attributes } ) => {
 		const { showLabel, useUrl } = attributes;
 
 		return (
-			<span
-				className="bh-wp-bitcoin-gateway-exchange-rate"
-				data-attribute-showLabel={ showLabel }
-				data-attribute-useUrl={ useUrl }
-			/>
+			<>
+				<span
+					className="bh-wp-bitcoin-gateway-exchange-rate"
+					data-attribute-showLabel={ showLabel }
+					data-attribute-useUrl={ useUrl }
+				/>
+			</>
 		);
 	},
 } );
