b53611f26467cd2153579e146b28ac596ee5cfd1
Brian Henry <BrianHenryIE@gmail.com>
2025-09-16 19:22:34 -0700

    Move `register_block` logic to `block.json`

--- includes/woocommerce/blocks/order-confirmation/class-bitcoin-exchange-rate-block.php
+++ includes/woocommerce/blocks/order-confirmation/class-bitcoin-exchange-rate-block.php
@@ -24,47 +24,8 @@ class Bitcoin_Exchange_Rate_Block {
 	 * @hooked init
 	 */
 	public function register_block(): void {
-
-		/** @var array{dependencies:array<string>, version:string} $webpack_asset_frontend */
-		$webpack_asset_frontend = include $this->settings->get_plugin_dir() . 'assets/js/frontend/woocommerce/blocks/order-confirmation/exchange-rate/exchange-rate-block.min.asset.php';
-		wp_register_script(
-			'bh-wp-bitcoin-gateway-exchange-rate-block',
-			$this->settings->get_plugin_url() . 'assets/js/frontend/woocommerce/blocks/order-confirmation/exchange-rate/exchange-rate-block.min.js',
-			$webpack_asset_frontend['dependencies'],
-			$webpack_asset_frontend['version'],
-			true
-		);
-
-		/** @var array{dependencies:array<string>, version:string} $webpack_asset_editor */
-		$webpack_asset_editor = include $this->settings->get_plugin_dir() . 'assets/js/frontend/woocommerce/blocks/order-confirmation/exchange-rate/exchange-rate-admin.min.asset.php';
-		wp_register_script(
-			'bh-wp-bitcoin-gateway-exchange-rate-admin',
-			$this->settings->get_plugin_url() . 'assets/js/frontend/woocommerce/blocks/order-confirmation/exchange-rate/exchange-rate-admin.min.js',
-			$webpack_asset_editor['dependencies'],
-			$webpack_asset_editor['version'],
-			true
-		);
-
 		register_block_type(
-			'bh-wp-bitcoin-gateway/exchange-rate',
-			array(
-				'editor_script' => 'bh-wp-bitcoin-gateway-exchange-rate-admin',
-				'attributes'    => array(
-					'orderId'   => array(
-						'type'    => 'number',
-						'default' => 0,
-					),
-					'showLabel' => array(
-						'type'    => 'boolean',
-						'default' => true,
-					),
-				),
-				'uses_context'  => array(
-					'bh-wp-bitcoin-gateway/btc_exchange_rate_formatted',
-					'bh-wp-bitcoin-gateway/exchange_rate_url',
-				),
-				'view_script'   => 'bh-wp-bitcoin-gateway-exchange-rate-block',
-			)
+			$this->settings->get_plugin_dir() . 'assets/js/frontend/woocommerce/blocks/order-confirmation/exchange-rate/'
 		);
 	}
 }
