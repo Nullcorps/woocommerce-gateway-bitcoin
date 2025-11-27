8c1eb47bafd954d73b3bc868f8e56842127683f5
Brian Henry <BrianHenryIE@gmail.com>
2025-11-26 22:50:11 -0800

    Improve PhpDoc

--- includes/api/model/class-addresses-generation-result.php
+++ includes/api/model/class-addresses-generation-result.php
@@ -17,11 +17,17 @@ use BrianHenryIE\WP_Bitcoin_Gateway\API\Addresses\Bitcoin_Wallet;
  *
  * @see API::generate_new_addresses()
  * @see API_Interface::generate_new_addresses()
+ *
+ * @phpstan-type Generated_Addresses_Array array{gateway_id:string, xpub:string, generated_addresses:array<Bitcoin_Address>, generated_addresses_count:int, generated_addresses_post_ids:array<int>, address_index:int}
  */
 class Addresses_Generation_Result {
 
 	/**
-	 * @param array{gateway_id:string, xpub:string, generated_addresses:array<Bitcoin_Address>, generated_addresses_count:int, generated_addresses_post_ids:array<int>, address_index:int} $new_addresses
+	 * Constructor
+	 *
+	 * @param Bitcoin_Wallet            $wallet The wallet that the new addresses were generated for.
+	 * @param Generated_Addresses_Array $new_addresses The newly generated addresses.
+	 * @param int                       $address_index The new highest wallet address index.
 	 */
 	public function __construct(
 		public Bitcoin_Wallet $wallet,
